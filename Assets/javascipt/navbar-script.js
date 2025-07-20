c634f9f69fceb54212b0e5e9b0d0024d03e0dbc2
iftikhar-00 <hussainiftikhar80@gmail.com>
2025-07-20 11:17:28 +0500

    asset folder deleted

--- Assets/javascipt/navbar-script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('menu-btn');
-const navLinks = document.getElementById('navLinks');
-
-hamburger.addEventListener('click', () => {
-navLinks.classList.toggle('hide');
-});
